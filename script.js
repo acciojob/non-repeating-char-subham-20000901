3bfa5a04910e11338310e561dd94394f7923f9d5
subham-20000901 <sinhasubh2000@gmail.com>
2025-12-23 23:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count ;
+	for(let i=0; i<str.length; i++){
+		  count = 0;
+		for(let j=0; j<str.length; j++){
+			if(str[i] === str[j]) count++;
+		}
+		if(count === 1) return str[i];
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+ const input = prompt("Enter a string");
+ alert(firstNonRepeatedChar(input)); 
+
